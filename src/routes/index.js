a51841cf02742bda88fd07e02f988e4f69acb0ba
Prismryu <guyrunby@gmail.com>
2025-04-21 17:12:06 -0400

    basic file structure added

---
+++ src/routes/index.js
@@ -0,0 +1,9 @@
+// routes/index.js
+const router = require('express').Router();
+const userRoutes = require('./api/userRoutes');
+const thoughtRoutes = require('./api/thoughtRoutes');
+
+router.use('/users', userRoutes);
+router.use('/thoughts', thoughtRoutes);
+
+module.exports = router;
